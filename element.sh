ce6ded33d9187f35dc31274585f7b2f3eeb2b435
Rizal De Vera <fcjaypee37@gmail.com>
2024-03-18 05:59:42 +0000

    fix: if argument is not in the database

--- element.sh
+++ element.sh
@@ -7,24 +7,51 @@ ELEMENTS=$($PSQL "select * from elements right join properties using(atomic_numb
 #ELEMENTS=$($PSQL "select * from elements;")
 
 
-# if no argument
-if [[ -z $1 ]]
- then
-  echo "Please provide an element as an argument."
- else
-  #number_symbol_name=$1
-  echo "$ELEMENTS" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID
-  do
-   if [[ $1 == $ATOMIC_NUMBER || $1 == $SYMBOL || $1 == $NAME ]]
+DISPLAY_PROPERTIES(){
+  # display properties of the element
+  #if [[ -z $ELEMENT_CHECKER ]]
+      #then
+      #echo "Argument is invalid or element is not yet in database."
+      #else
+      echo "$ELEMENTS" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID
+      do
+        if [[ $USER_INPUT == $ATOMIC_NUMBER || $USER_INPUT == $SYMBOL || $USER_INPUT == $NAME ]]
+          then
+          echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+          break
+        fi
+      done
+  #fi
+}
+
+ELEMENT_CHECKER(){
+  if [[ $USER_INPUT =~ ^[0-9]+$ ]]
     then
-      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-      break
+    ELEMENT_CHECK=$($PSQL "select atomic_number from elements where atomic_number = $USER_INPUT;")
+    if [[ -z $ELEMENT_CHECK ]]
+      then
+      echo "I could not find that element in the database."
+    fi
     else
-      echo "Argument is invalid or element is not yet in database."
-      break
-   fi
-  done
+    ELEMENT_CHECK=$($PSQL "select atomic_number from elements where symbol = '$USER_INPUT' or name = '$USER_INPUT';")
+    if [[ -z $ELEMENT_CHECK ]]
+      then
+      echo "I could not find that element in the database."
+    fi
+  fi
+}
 
+if [[ -z $1 ]]
+  then
+  echo "Please provide an element as an argument."
+  else
+  
+  # set argument into USER_INPUT
+  USER_INPUT=$1
+  
+  # check if argument is in database
+  #ELEMENT_CHECKER=$($PSQL "select atomic_number from elements where atomic_number = $USER_INPUT or symbol = '$USER_INPUT' or name = '$USER_INPUT';")
+  ELEMENT_CHECKER
+  DISPLAY_PROPERTIES
 fi
-
 
