73b991d266c2bf07abbbde04f36a9f1f3b8d53a5
Rizal De Vera <fcjaypee37@gmail.com>
2024-03-18 06:37:33 +0000

    fix: element checker

--- element.sh
+++ element.sh
@@ -4,36 +4,33 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t -c"
 
 # get elements and properties
 ELEMENTS=$($PSQL "select * from elements right join properties using(atomic_number);")
+
 #ELEMENTS=$($PSQL "select * from elements;")
 
 
 DISPLAY_PROPERTIES(){
-  # display properties of the element
-  #if [[ -z $ELEMENT_CHECKER ]]
-      #then
-      #echo "Argument is invalid or element is not yet in database."
-      #else
-      echo "$ELEMENTS" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID
-      do
-        if [[ $USER_INPUT == $ATOMIC_NUMBER || $USER_INPUT == $SYMBOL || $USER_INPUT == $NAME ]]
-          then
-          echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-          break
-        fi
-      done
-  #fi
+
+  echo "$ELEMENT_CHECK" | while read TYPE_IC BAR ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE
+    do
+      if [[ $USER_INPUT == $ATOMIC_NUMBER || $USER_INPUT == $SYMBOL || $USER_INPUT == $NAME ]]
+        then
+        echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+        break
+      fi
+    done
+
 }
 
 ELEMENT_CHECKER(){
   if [[ $USER_INPUT =~ ^[0-9]+$ ]]
     then
-    ELEMENT_CHECK=$($PSQL "select atomic_number from elements where atomic_number = $USER_INPUT;")
+    ELEMENT_CHECK=$($PSQL "select * from elements right join properties using(atomic_number) full join types using(type_id) where atomic_number=$USER_INPUT;")
     if [[ -z $ELEMENT_CHECK ]]
       then
       echo "I could not find that element in the database."
     fi
     else
-    ELEMENT_CHECK=$($PSQL "select atomic_number from elements where symbol = '$USER_INPUT' or name = '$USER_INPUT';")
+    ELEMENT_CHECK=$($PSQL "select * from elements right join properties using(atomic_number) full join types using(type_id) where symbol = '$USER_INPUT' or name = '$USER_INPUT';")
     if [[ -z $ELEMENT_CHECK ]]
       then
       echo "I could not find that element in the database."
@@ -48,9 +45,6 @@ if [[ -z $1 ]]
   
   # set argument into USER_INPUT
   USER_INPUT=$1
-  
-  # check if argument is in database
-  #ELEMENT_CHECKER=$($PSQL "select atomic_number from elements where atomic_number = $USER_INPUT or symbol = '$USER_INPUT' or name = '$USER_INPUT';")
   ELEMENT_CHECKER
   DISPLAY_PROPERTIES
 fi
